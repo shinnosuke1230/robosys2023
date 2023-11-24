89f1980d6c42e65d7074a8ac58b7bf714136fb3c
Sito Shinnosuke <sitsnk@icloud.com>
2023-11-24 14:27:52 +0900

    Write copyright and license notice

--- test1.bash
+++ test1.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Shinnosuke Saito
+# SPDX-License-Identifier: GPL-3.0
 
+ng () {
+      echo NG at Line $1
+      res=1
+
+}
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus.py)
+[ "${out}" = 15 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK
+exit $res
 
